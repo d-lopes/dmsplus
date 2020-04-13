2270c7cbe02b673b0f74ccf7b8b603b006cca9ae
dlopes <dominique.lopes@mhp.com>
2020-04-13 16:14:38 +0200

    final adjustments before release 1.0.0

--- laravel/docker-entrypoint.sh
+++ laravel/docker-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# on initial startup generate the laravel app key
+INIT_INDICATOR_FILE='/var/www/storage/initialized'
+if ! [[ -f "$INIT_INDICATOR_FILE" ]]; then 
+  php artisan key:generate
+  echo "true" >> $INIT_INDICATOR_FILE
+fi
+
 # wait for mysql
 while ! nc -vz $DB_HOST 3306 ; do
   sleep 1
