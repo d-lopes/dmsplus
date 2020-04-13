96cf98cfa381435e1832f3115e8f0655f36f6f78
dlopes <dominique.lopes@mhp.com>
2020-04-13 16:46:43 +0200

    fix environment variables and issues on start-up
    Co-authored-by: dlopes <dominique.lopes@mhp.com>

---
+++ laravel/.valet-env.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'dms-plus' => [ // Only applies to app-name.test
+        'MYSQL_DATABASE' => 'dms_web',
+        'MYSQL_USER' => 'dms',
+        'MYSQL_PASSWORD' => 'dms_secret'
+	],
+];
