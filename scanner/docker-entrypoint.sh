b9054020dc47b56c31737e8c6c304ab49d9d9df2
dlopes <dominique.lopes@mhp.com>
2020-05-03 20:38:00 +0200

    automatic ingestion of new PDF files increased to every 15 minutes in order to mitigate [issue #12]

--- scanner/docker-entrypoint.sh
+++ scanner/docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # capture environment variables in order to allow provising them to child processes run by cronjobs
+if [ -f "/env.sh" ]; then
+    rm /env.sh 
+fi
 printenv | sed 's/^\(.*\)$/export \1/g' > /env.sh
 
-# Setup a cron schedule
-echo "* * * * * flock -xn /home/scanner/watch.lck -c '. /env.sh; /home/scanner/watch.sh'
-# This extra line makes it a valid cron" > scheduler.txt
+# Setup a cron schedule: see https://crontab.guru/ for help
+if [ ! -f "scheduler.txt" ]; then
+    echo "0/15 * * * * flock -xn /home/scanner/watch.lck -c '. /env.sh; /home/scanner/watch.sh'
+    # This extra line makes it a valid cron" > scheduler.txt
+fi
 
 # Start the run once job.
 echo "OCR scanner has been started"
