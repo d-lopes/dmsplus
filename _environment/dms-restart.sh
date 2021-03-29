00aa706f9b34cff44169866393f1939a828941b4
Lopes, Dominique (EXTERN: MHP) <dominique.lopes@mhp.com>
2021-03-29 19:43:05 +0200

    further restart script optimizations --
    - allow alternative versions for docker compose file
    - fixiate docker project name (for static volume/network prefixes)

--- _environment/dms-restart.sh
+++ _environment/dms-restart.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+# initialization
 CURR_DIR=`dirname "$0"`
 CONFIG_FILE="dms.env"
+COMPOSE_FILE="$CURR_DIR/docker-compose.yml"
+
+# make sure environment file for docker compose is present
 if [ ! -f "$CURR_DIR/$CONFIG_FILE" ]; then
     echo "$CONFIG_FILE does not exist in directory $CURR_DIR. Please copy $CONFIG_FILE.example and adjust it to your needs."
     exit -4711
 fi
 
+# allow alternative versions for docker compose file
+if [ ! -z "$1" ]; then
+    COMPOSE_FILE="$1"
+    if [ ! -f "$COMPOSE_FILE" ]; then
+        echo "$1 does not exist. Please reference an existing docker compose file."
+        exit -0815
+    fi  
+fi
+
 # stop and remove docker containers
-docker-compose -f "$CURR_DIR/docker-compose.yml" down
+docker-compose -p dms -f "$COMPOSE_FILE" down
 
 # remove volume (as it might contain outdated php files)
 docker volume rm dms_webdata
 
 # start up docker containers
-docker-compose -f "$CURR_DIR/docker-compose.yml" up -d
+docker-compose -p dms -f "$COMPOSE_FILE" up -d
