2952b70649c8e85665690cba7a36c693372fe6bd
Lopes, Dominique (EXTERN: MHP) <dominique.lopes@mhp.com>
2021-03-30 17:44:34 +0200

    remove legacy code (from before Laravel 8 migration)

--- laravel/app/Http/Controllers/DocumentController.php
+++ laravel/app/Http/Controllers/DocumentController.php
@@ -25,44 +25,4 @@ class DocumentController extends Controller
         ]);
     }
 
-    public function edit(Request $request, $id) {
-        $document = Document::find($id);
-        if ($document === null) {
-            throw new ModelNotFoundException("document with Id $id does not exist");
-        }
-        $content = $request->input('content');
-        $document->content = $content;
-        $document->saveAndUpdateStatus();        
-
-        return view('documents.single-view', [
-            'document' => $document,
-            'searchterm' => $request->st
-        ]);
-    }
-
-    public function addFile(Request $request, $id) {
-        $uploadedFile = $request->file('file');
-        if ($uploadedFile === null) {
-            throw new InvalidRequestException("no file in field 'file' submitted");
-        }
-        
-        $document = Document::find($id);
-        if ($document === null) {
-            throw new ModelNotFoundException("resource with Id $id does not exist");
-        }
-        
-        // save file
-        $currentDate =  date('Y-m-d');
-        $path = $uploadedFile->store($currentDate, ['disk' => 'documents']);
-
-        // update document path and status in DB
-        $document->path = $path;
-        $document->saveAndUpdateStatus();
-
-        return view('documents.single-view', [
-            'document' => $document,
-            'searchterm' => $request->st
-        ]);
-    }
-
 }
